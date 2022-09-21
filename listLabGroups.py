cf4921245fedf2c647c8df769fecd418d0eedeec
Seth McNeill <seth.mcneill@erau.edu>
2022-09-21 15:28:48 -0700

    Try 2

--- listLabGroups.py
+++ listLabGroups.py
@@ -14,6 +14,35 @@ import argparse     # This gives better command line argument functionality
 from itertools import combinations  # creates combinations based on list and n
 from numpy import random  # for permutations
 
+
+def evenGroups(nameList):
+    '''Creates pairs of names that don't overlap and 
+       are only used once for a name list with an even
+       number of names'''
+    nNames = len(nameList)
+    if nNames % 2: # odd number of names
+        print("Error: Odd number of names")
+        #return(-1)
+    allGroups = []
+    for ii in range(1,nNames):
+        groups = list()
+        for jj in range(0,nNames):
+            curName = nameList[jj]
+            nextName = nameList[(jj+ii)%nNames] 
+            print(f'{curName}, {nextName}', end='')
+            usedNames = [name for group in groups for name in group]
+            if curName not in usedNames and nextName not in usedNames:
+                groups.append((curName, nextName))    
+                print('*')
+            else:
+                print('')
+        if len(groups) != nNames/2:
+            print(f'incomplete groups: {groups}')
+        print(groups)
+        pdb.set_trace()
+        allGroups.append(groups)
+    return(allGroups)
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('-f','--namefile',
@@ -28,66 +57,31 @@ if __name__ == '__main__':
     nameList = [n.strip() for n in nameList]
     nNames = len(nameList)
     print(f'#names: {nNames}')
-    nameCombsIter = combinations(nameList, 2)  # get the combinations 
-    nameCombs = [namePair for namePair in nameCombsIter]  # convert to list for operating on
-    random.shuffle(nameCombs)
-    for nameComb in nameCombs:
-        print(nameComb)
+    randNames = random.permutation(nameList)
+    print(randNames)
+
+    randNames = nameList
     
+    # https://www.geeksforgeeks.org/python-ways-to-rotate-a-list/
     if nNames % 2:  # odd number of names
         print("Odd number of names")
-        randNames = random.permutation(nameList)
-        print(randNames)
-        labList = []
-        soloPerson = randNames[0]
-        usedNames = [soloPerson]
-        randNames = randNames[1:]
-        labNum = 1  # lab count number
-        while nameCombs[:]:
-            # put in combinations that don't include soloPerson
-            # add soloPerson as last group
-            # increment soloPerson
-            # reset labList and usedNames
-            for names in nameCombs:
-                # check to see if any names in names are in usedNames
-                # if not, add names to labList and remove from nameCombs
-                if not any(n in usedNames for n in names):
-                    usedNames.extend(names)
-                    labList.append(names)
-                    nameCombs.remove(names)
-            labList.append((soloPerson))
-            print(f'Lab {labNum}:\n {labList}')
-            labNum += 1
-            labList = []
-            if len(randNames) == 0:
-                print(nameCombs)
-                break
-            soloPerson = randNames[0]
-            usedNames = [soloPerson]
-            randNames = randNames[1:]
+        # check whether user wants groups of 1 or 3
+        allGroups = evenGroups(randNames)
+        ii = 0
+        for groups in allGroups:
+            usedNames = set([name for group in groups for name in group])
+            unused = set(randNames).difference(usedNames).pop()
+            groups.append(unused)
+            print(f'Lab {ii}: {groups}')
+            ii += 1        
     else:
-        # try to build lab partners for each class
-        # https://flexiple.com/python/check-if-list-is-empty-python/
-        labList = []
-        usedNames = []
-        labNum = 1  # lab count number
-        while nameCombs[:]:  # while nameCombs isn't empty
-            # start the lab list with the first element still in the list
-            if not labList:  # labList is empty
-                usedNames.extend(nameCombs[0])
-                labList.append(nameCombs.pop(0))
-            # Add tuples that don't contain the names already in the list
-            for names in nameCombs:
-                # check to see if any names in names are in usedNames
-                # if not, add names to labList and remove from nameCombs
-                if not any(n in usedNames for n in names):
-                    usedNames.extend(names)
-                    labList.append(names)
-                    nameCombs.remove(names)
-            print(f'Lab {labNum}:\n {labList}')
-            labNum += 1
-            labList = []
-            usedNames = []
+        print("Even number of names")
+        groups = evenGroups(randNames)
+        ii = 0
+        for group in groups:
+            print(f'Lab {ii}: {group}')
+            ii += 1
+
 
     end_time = datetime.datetime.now()    # save the script end time
     print(f'{__file__} took {end_time - start_time} s to complete')
